fca49b51162e9beab1625c51f02f378deac5c007
claudio Abud <clau@gmail.com>
2022-03-22 23:16:59 -0500

    refactored code to use custom hooks to wrap the useMemo and useEffect hooks

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -2,31 +2,18 @@ import logo from './logo.svg';
 import './main-page.css';
 import Header from './header';
 import FeatureHouse from './featured-house';
-import { useEffect, useState, useMemo } from 'react';
+import useHouses from '../hooks/useHouses';
 import {BrowserRouter, Routes, Route, } from 'react-router-dom'
 import SearchResults from '../search-results'
 import HouseFilter from './house-filter';
 import HouseFromQuery from '../house/HouseFromQuery';
+import useFeaturedHouse from '../hooks/useFeaturedHouse';
 
 function App() {
-  const [allHouses, setAllHouses] = useState([])
+  
+  const allHouses = useHouses();
+  const featuredHouse = useFeaturedHouse(allHouses);
 
-  useEffect(() => {
-      const fetchHouses = async () => {
-        const response = await fetch("/houses.json")
-        const houses = await response.json()
-        setAllHouses(houses)
-      }
-      fetchHouses()
-  }, [])
-
-  const featuredHouse = useMemo(() => {
-    if (allHouses.length) {
-      let randomIndex = Math.floor(Math.random() * allHouses.length)
-      return allHouses[randomIndex]
-    }
-  },
-  [allHouses])
 
   return (
     <BrowserRouter>
